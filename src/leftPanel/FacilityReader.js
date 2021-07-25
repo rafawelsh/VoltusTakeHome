868c67ca2bfa98658365e37ea336d9701d65e790
rafawelsh <mrrafael1912@gmail.com>
2021-07-24 17:41:21 -0700

    added form with options and input

--- src/leftPanel/FacilityReader.js
+++ src/leftPanel/FacilityReader.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function FacilityReader(name) {
-    if (!name) return null
+function FacilityReader(places) {
+    const [input, setInput] = useState('')
+    const [location, setLocation] = useState('')
+    if (!places.places) return null
+    console.log(places)
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleSelect = (e) => {
+        setLocation(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        console.log(input)
+        console.log(location)
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            <p>{name}</p>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <select name="cars" id="cars" value={location} onChange={handleSelect}>
+                <option value="none" selected>
+                    Select
+                </option>
+            {places.places.map(location => {
+                return (
+                    <option key={location.id} value={location.name} >{location.name}</option>
+                )
+            })}
+            </select>
+
+            <input type="text" value={input} placeholder="kw" onChange={handleChange} />
+            <input type="submit" value="Submit" />
+        </form>
     )
 }
 
