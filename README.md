250d7a58553ec9548f35621ba62b4d3279c8adda
rafawelsh <mrrafael1912@gmail.com>
2021-07-26 10:35:57 -0700

    last minute realization, passed all set of tasks

--- README.md
+++ README.md
@@ -14,15 +14,13 @@ We're getting the green light! Voltus is building a command center dashboard for
 # Set of Tasks to Accomplish
 - [x] Fetch data
 - [x] Display facilities' locations on a map
-- [] Add a side panel to the left of the map. 
+- [x] Add a side panel to the left of the map. 
     - [x] 1. Insert `Facility, Reading, Last Update`
     The facility column should just contain the name of the facility. For now the reading column can just be defaulted to 0 or 0kW or random values. And the Last Updated can be defaulted to any value.
-    - []2. The top part should have inputs for the user to select a facility and input a kW value. This should update the values of `Reading` and `Last Update` in the table, as well as the total.
+    - [x]2. The top part should have inputs for the user to select a facility and input a kW value. This should update the values of `Reading` and `Last Update` in the table, as well as the total.
 - [x] 4. The header should read the name of the organization
 
 
 ## Issues
-- I had issues updating the kilowatts reading from the input field inside the LeftPanel component. Since the object fetched from the API didn't include an `kilowatts` property, I decided to add it manually when called. I was able to then print out the userInput but whenever I attempted to change the state of `place.kilowatts` is didn't work.
-- I didn't have enough time to work on the time update field within the table
 - I did write `FacilityReader` to originally hold the top half of the `LeftPanel` component. After experiencing difficulties updating state, I had to lift the state up to `LeftPanel`. I did leave `FacilityReader` so you all can see my train of thought.
 
--- src/leftPanel/LeftPanel.js
+++ src/leftPanel/LeftPanel.js
@@ -6,16 +6,21 @@ function LeftPanel(facilities) {
     const [location, setLocation] = useState('')
     const [input, setInput] = useState('')
     const [kw, setKW] = useState(0)
+    let totalKW = 0
    
     if (!facilities.facilities) return null
-  
+
     places.map((place) => {
-        place.kilowatts = 0
+        if (!place.kilowatts) {
+            place.kilowatts = 0
+        }
+        if (!place.lastUpdate) {
+            const date = new Date("2018-07-25T11:00:00.000Z")
+            place.lastUpdate = date.toUTCString()
+        }
     })
 
-    /*
-    * Attmpting to set `kw` to the input to then update the place.kilowatts
-    */
+   
     const handleChange = (e) => {
         setInput(e.target.value)
         const number = parseInt(e.target.value)
@@ -26,28 +31,24 @@ function LeftPanel(facilities) {
         setLocation(e.target.value)
     }
 
-    /*
-     * This handleSubmit would be the place where the Reading and Time are updated.
-     * Difficulties writing the code to update those props
-     * It's suppose to find the matching place and then we updated the whole Array of `places` 
-     * and update with setPlaces
-     */
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        let date = new Date()
+        date = date.toUTCString()
+       
         const newPlaces = places.map((place) => {
             if (place.name === location) {
-                const updatedPlace = {
-                    ...place,
-                    kilowatts: {kw}
-                }
-                return updatedPlace
+                place.kilowatts = kw
+                place.lastUpdate = date
             }
             return place
         })
         setPlaces(newPlaces)
     }
+
     return (
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent:'space-around'}}>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent:'space-around', minWidth: '25vw'}}>
             <form onSubmit={handleSubmit}>
                 <select name="locations" id="locations" value={location} onChange={handleSelect}>
                     <option value="none" defaultValue>
@@ -74,20 +75,21 @@ function LeftPanel(facilities) {
                 </thead>
                 <tbody>
                 {places.map((facility) => {
+                    totalKW += facility.kilowatts
                     return (
                         <tr key={facility.id} 
                         style={ facility.id%2===0 ? { backgroundColor:'white'} : {backgroundColor:'#007eff47'}}>
                             <td>{facility.name}</td>
                             <td>{facility.kilowatts} kw</td>
-                            <td>time</td>
+                            <td>{facility.lastUpdate}</td>
                         </tr>
                     )        
                 })}
                 </tbody>
-                <tfoot style={{backgroundColor: 'green'}}>
+                <tfoot style={{backgroundColor: 'limegreen'}}>
                     <tr>
                         <td>All</td>
-                        <td>0 kw</td>
+                        <td>{totalKW} kw</td>
                         <td> </td>
                     </tr>
                 </tfoot>
